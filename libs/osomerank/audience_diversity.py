546f687cbde40190da0921b2acd2f79ed955aa75
Saumya Bhadani <saumya@pop-os.localdomain>
2024-05-17 14:46:26 -0400

    Added audience diversity of multiple posts

--- libs/osomerank/audience_diversity.py
+++ libs/osomerank/audience_diversity.py
@@ -11,6 +11,7 @@ import pandas as pd
 import re
 import requests
 import json
+import traceback
 
 import os
 
@@ -39,6 +40,16 @@ def process_URL(url):
     except Exception:
         return url
 
+def process_URL_multiple(urls):
+    res = []
+    for url in urls: 
+        try:
+            r = requests.head(url, allow_redirects=True,timeout=10)
+            res.append(r.url)
+        except Exception:
+            res.append(url)
+    return res
+
 def remove_urls(text, replacement_text=""):
     # Define a regex pattern to match URLs
     url_pattern = re.compile(r'https?://\S+|www\.\S+')
@@ -55,6 +66,95 @@ def process_text(text):
         return "NA"
     return text_urls_removed
 
+#Filtering text: remove special characters, alphanumeric, return None if text doesn't meet some criterial like just one word or something
+def process_text_multiple(texts):
+    processed_texts = []
+    for text in texts:
+        text_urls_removed = remove_urls(text)
+        if len(text_urls_removed.strip().split(" ")) <= 3:
+            processed_texts.append("NA")
+        else:
+            processed_texts.append(text_urls_removed)
+    return processed_texts
+
+def audience_diversity_multiple(feed_posts,sm_type):
+    urls_available = []
+    urls_index = []
+    
+    audience_diversity_val = [-1000]*len(feed_posts)
+    
+    try:
+        
+        if sm_type == 'twitter':
+            for i in range(len(feed_posts)):
+                feed_post = feed_posts[i]
+                if feed_post["expanded_url"]:
+                    urls_index.append(i)
+                    urls_available.append(feed_post["expanded_url"])
+        
+        else:
+            for i in range(len(feed_posts)):
+                feed_post = feed_posts[i]
+                if "text" in feed_post.keys():
+                    if type(feed_post["text"]) != float:
+                        if URL_from_text(feed_post["text"]) != "NA":
+                            urls_index.append(i)
+                            urls_available.append(URL_from_text(feed_post["text"]))
+        
+        urls_available_unshortened = process_URL_multiple(urls_available)
+        
+        for i in range(len(urls_available_unshortened)):
+            url_available = urls_available_unshortened[i]
+            domain = ".".join(url_available.split("/")[2].split(".")[-2:])
+            if domain in audience_diversity_domains:
+                audience_diversity_val[urls_index[i]] = pd_audience_diversity_URLs.loc[pd_audience_diversity_URLs['private_domain'] == domain]['visitor_var'].values[0]
+        
+        sm_texts = []
+        text_index = []
+        
+        for i in range(len(feed_posts)):
+            if audience_diversity_val[i] == -1000:
+                feed_post = feed_posts[i]
+                if sm_type == 'reddit':
+                    if 'title' in feed_post.keys() and 'text' in feed_post.keys():
+                        sm_texts.append(feed_post['title'] + ". " + feed_post["text"])
+                        text_index.append(i)
+                    elif 'title' in feed_post.keys():
+                        sm_texts.append(feed_post['title'])
+                        text_index.append(i)
+                    elif 'text' in feed_post.keys():
+                        sm_texts.append(feed_post['text'])
+                        text_index.append(i)
+                else:
+                    if type(feed_post["text"]) != float:
+                        sm_texts.append(feed_post['text'])
+                        text_index.append(i)
+        
+        sm_texts_processed = process_text_multiple(sm_texts)
+        texts_processed = []
+        text_index_processed = []
+        
+        for i in range(len(sm_texts_processed)):
+            if sm_texts_processed[i] != "NA":
+                texts_processed.append(sm_texts_processed[i])
+                text_index_processed.append(text_index[i])
+        
+        topics = BERTopic_model_loaded.transform(texts_processed)[0]
+        
+        for i in range(len(topics)):
+            if int(topics[i]) != -1:
+                audience_diversity_val[text_index_processed[i]] = topic_diversity[str(topics[i])]
+                
+    except Exception:
+        print(traceback.format_exc())
+        return mean_topic_diversity
+
+    for i in range(len(audience_diversity_val)):
+        if audience_diversity_val[i] == -1000:
+            audience_diversity_val[i] = mean_topic_diversity
+    
+    return audience_diversity_val
+
 def audience_diversity(feed_post,sm_type):
     url_available = ''
     
@@ -70,7 +170,7 @@ def audience_diversity(feed_post,sm_type):
                 url_available = URL_from_text(feed_post["text"])
         
         if url_available:
-            url_available = process_URL(url_available)
+            #url_available = process_URL(url_available)
             domain = ".".join(url_available.split("/")[2].split(".")[-2:])
             if domain in audience_diversity_domains:
                 audience_diversity_val = pd_audience_diversity_URLs.loc[pd_audience_diversity_URLs['private_domain'] == domain]['visitor_var'].values[0]
@@ -90,7 +190,7 @@ def audience_diversity(feed_post,sm_type):
                 sm_text = process_text(sm_text)
                 topic = BERTopic_model_loaded.transform([sm_text])[0][0]
                 if int(topic) != -1:
-                    audience_diversity_val = topic_diversity[int(topic)]
+                    audience_diversity_val = topic_diversity[str(topic)]
     except Exception as e:
         print(e)
         return mean_topic_diversity
