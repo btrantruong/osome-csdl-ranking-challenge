f76168c48c49bda6610ead779658b764d26c8864
Bao BigRed <btrantruong@gmail.com>
2024-06-13 11:32:20 -0400

    simplify AD

--- libs/osomerank/audience_diversity.py
+++ libs/osomerank/audience_diversity.py
@@ -16,8 +16,8 @@ import re
 import requests
 import json
 import traceback
-from unshorten_URLs import unshorten_main
-
+from osomerank.unshorten_URLs import unshorten_main
+import numpy as np
 import os
 import configparser
 
@@ -37,12 +37,16 @@ audience_diversity_domains = (
 )
 
 BERTopic_model_loaded = BERTopic.load(
-    os.path.join(libs_path, config.get("AUDIENCE_DIVERSITY", "audience_diversity_rockwell"))
+    os.path.join(
+        libs_path, config.get("AUDIENCE_DIVERSITY", "audience_diversity_rockwell")
+    )
 )
 # BERTopic_model_loaded = BERTopic.load()
 
 topic_diversity = {}
-with open(os.path.join(libs_path, config.get("AUDIENCE_DIVERSITY", "topic_diversity_json"))) as ff:
+with open(
+    os.path.join(libs_path, config.get("AUDIENCE_DIVERSITY", "topic_diversity_json"))
+) as ff:
     topic_diversity = json.load(ff)
 
 mean_topic_diversity = 0.17
@@ -83,114 +87,116 @@ def remove_urls(text, replacement_text=""):
     return text_without_urls
 
 
-# Filtering text: remove special characters, alphanumeric, return None if text doesn't meet some criterial like just one word or something
 def process_text(text):
+    # Filtering text: remove special characters, alphanumeric, return None if text doesn't meet some criterial like just one word or something
     text_urls_removed = remove_urls(text)
     if len(text.strip().split(" ")) <= 3:
         return "NA"
     return text_urls_removed
 
 
-# Filtering text: remove special characters, alphanumeric, return None if text doesn't meet some criterial like just one word or something
-def process_text_multiple(texts):
-    processed_texts = []
-    for text in texts:
-        text_urls_removed = remove_urls(text)
-        if len(text_urls_removed.strip().split(" ")) <= 3:
-            processed_texts.append("NA")
-        else:
-            processed_texts.append(text_urls_removed)
-    return processed_texts
+def get_reddit_text(post):
+    if "title" in post.keys() and "text" in post.keys():
+        text = post["title"] + ". " + post["text"]
+    elif "title" in post.keys():
+        text = post["title"]
+    elif "text" in post.keys():
+        text = post["text"]
+    return text
 
 
-def audience_diversity_multiple(feed_posts, sm_type):
+def audience_diversity_multiple(feed_posts, platform):
+    """
+    Calculates the Audience Diversity (AD) and Topic Diversity (TD) scores for social media timeline.
+
+    Parameters:
+    feed_posts (list of json objects): the timeline
+    platform (str): the type of social media: {'twitter', 'reddit', 'facebook'}
+
+    Returns:
+    AD/TD scores (list of float): The predicted AD (if post contains URLs) or TD (if not) scores for a list of posts
+    """
+
+    ## Preprocess posts
+
     urls_available = []
     urls_index = []
 
-    audience_diversity_val = [-1000] * len(feed_posts)
+    sm_texts = []
+    text_index = []
 
-    try:
+    audience_diversity_val = np.empty(len(feed_posts))
+    audience_diversity_val.fill(-1000)
 
-        if sm_type == "twitter":
-            for i in range(len(feed_posts)):
-                feed_post = feed_posts[i]
-                if feed_post["embedded_urls"]:
-                    for urll in feed_post["embedded_urls"]:
-                        urls_index.append(i)
-                        urls_available.append(urll)
-
+    # keep track of index of post with/without URLs
+    for i, post in enumerate(feed_posts):
+        # Finding URLs is straightforward for twitter
+        if platform == "twitter":
+            if post["embedded_urls"]:
+                for urll in post["embedded_urls"]:
+                    urls_index.append(i)
+                    urls_available.append(urll)
         else:
-            for i in range(len(feed_posts)):
-                feed_post = feed_posts[i]
-                if "text" in feed_post.keys():
-                    if type(feed_post["text"]) != float:
-                        if URL_from_text(feed_post["text"]) != "NA":
-                            urls_index.append(i)
-                            urls_available.append(URL_from_text(feed_post["text"]))
-
-        if urls_available:
-            urls_available_unshortened = unshorten_main(urls_available)
-            #urls_available_unshortened = process_URL_multiple(urls_available)
-        else:
-            urls_available_unshortened = []
-
-        for i in range(len(urls_available_unshortened)):
-            url_available = urls_available_unshortened[i]
-            domain = ".".join(url_available.split("/")[2].split(".")[-2:])
-            if 'twitter' in domain:
-                continue
-            if domain in audience_diversity_domains:
-                audience_diversity_val[urls_index[i]] = pd_audience_diversity_URLs.loc[
-                    pd_audience_diversity_URLs["private_domain"] == domain
-                ]["visitor_var"].values[0]
-
-        sm_texts = []
-        text_index = []
-
-        for i in range(len(feed_posts)):
-            if audience_diversity_val[i] == -1000:
-                feed_post = feed_posts[i]
-                if sm_type == "reddit":
-                    if "title" in feed_post.keys() and "text" in feed_post.keys():
-                        sm_texts.append(feed_post["title"] + ". " + feed_post["text"])
-                        text_index.append(i)
-                    elif "title" in feed_post.keys():
-                        sm_texts.append(feed_post["title"])
-                        text_index.append(i)
-                    elif "text" in feed_post.keys():
-                        sm_texts.append(feed_post["text"])
-                        text_index.append(i)
+            if "text" in post.keys() and type(post["text"]) != float:
+                # finding URLs for other platforms
+                if URL_from_text(post["text"]) != "NA":
+                    urls_index.append(i)
+                    urls_available.append(URL_from_text(post["text"]))
+                # posts without URLs:
                 else:
-                    if type(feed_post["text"]) != float:
-                        sm_texts.append(feed_post["text"])
-                        text_index.append(i)
+                    if platform == "reddit":
+                        sm_texts.append(get_reddit_text(post))
+                    else:
+                        sm_texts.append(post["text"])
+                    text_index.append(i)
 
-        sm_texts_processed = process_text_multiple(sm_texts)
-        texts_processed = []
-        text_index_processed = []
+    ## Predict AD/TD scores
+    # posts with URLs: get AD
+    # first, unshorten URLs
 
-        for i in range(len(sm_texts_processed)):
-            if sm_texts_processed[i] != "NA":
-                texts_processed.append(sm_texts_processed[i])
-                text_index_processed.append(text_index[i])
+    if urls_available:
+        urls_available_unshortened = unshorten_main(urls_available)
+        # urls_available_unshortened = process_URL_multiple(urls_available)
+    else:
+        urls_available_unshortened = []
 
-        topics = BERTopic_model_loaded.transform(texts_processed)[0]
+    # fill in AD scores, preserving index
+    ad_scores = []  # ad score index = urls_index
 
-        for i in range(len(topics)):
-            if int(topics[i]) != -1:
-                audience_diversity_val[text_index_processed[i]] = topic_diversity[
-                    str(topics[i])
-                ]
+    for url_available in urls_available_unshortened:
+        domain = ".".join(url_available.split("/")[2].split(".")[-2:])
+        if "twitter" in domain:
+            continue
+        if domain in audience_diversity_domains:
+            ad_score = pd_audience_diversity_URLs.loc[
+                pd_audience_diversity_URLs["private_domain"] == domain
+            ]["visitor_var"].values[0]
+            ad_scores.append(ad_score)
 
-    except Exception:
-        print(traceback.format_exc())
-        return mean_topic_diversity
+    # posts without URLs: get TD
+    # first, clean text
+    texts_processed = []
+    text_index_processed = []
+    for i, text in enumerate(sm_texts):
+        clean_text = process_text(text)
+        if clean_text != "NA":
+            texts_processed.append(clean_text)
+            text_index_processed.append(text_index[i])
 
-    for i in range(len(audience_diversity_val)):
-        if audience_diversity_val[i] == -1000:
-            audience_diversity_val[i] = mean_topic_diversity
+    topics = BERTopic_model_loaded.transform(texts_processed)[0]
 
+    td_scores = []
+    for i in range(len(topics)):
+        if int(topics[i]) != -1:
+            td_score = topic_diversity[str(topics[i])]
+        else:
+            td_score = mean_topic_diversity
+        td_scores.append(td_score)
+
+    audience_diversity_val[urls_index] = ad_scores
+    audience_diversity_val[text_index_processed] = td_scores
     return audience_diversity_val
+
 
 def audience_diversity(feed_post, sm_type):
     url_available = ""
@@ -238,23 +244,3 @@ def audience_diversity(feed_post, sm_type):
         audience_diversity_val = mean_topic_diversity
 
     return audience_diversity_val
-
-
-# if __name__ == "__main__":
-#     sample_post_twitter = {}
-#     with open('sample_posts/sample_post_twitter.json') as ff:
-#         sample_post_twitter = json.load(ff)
-#     print("Audience Diversity for Twitter : ")
-#     print(audience_diversity(sample_post_twitter, "twitter"))
-
-#     sample_post_facebook = {}
-#     with open('sample_posts/sample_post_facebook.json') as ff:
-#         sample_post_facebook = json.load(ff)
-#     print("Audience Diversity for Facebook : ")
-#     print(audience_diversity(sample_post_facebook, "facebook"))
-
-#     sample_post_reddit = {}
-#     with open('sample_posts/sample_post_reddit.json') as ff:
-#         sample_post_reddit = json.load(ff)
-#     print("Audience Diversity for Reddit : ")
-#     print(audience_diversity(sample_post_reddit, "reddit"))
