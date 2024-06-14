72e4dfe98d2be42a7a6c4fb170282fbca5b161e3
btrantruong <baotruon@iu.edu>
2024-06-15 01:45:03 +0700

    Update ranker: latest AD_rockwell, batch mode (#7)
    * fix ranker to return & save raw predictions
    
    * ranker profileit timing
    
    * add profiling
    
    * ranker time profiling
    
    * ranker_profile simplify & test
    
    * ranker test scripts
    
    * AD model
    
    * data & benchmark results
    
    * batch mode for prediction (ER libs & app)
    
    * analyze corr between AD & HaR
    
    * cleanup
    
    * format
    
    * simplify AD
    
    * ranker passed pytest
    
    * updated main ranker with batch mode
    
    * audience diversity: change function names, reverted to latest ad_multiple
    
    * model fpaths, er setup without gpu
    
    * cleanup
    
    * ranking server test file & data
    
    * gitignore
    
    * cleanup
    
    * cleanup (rm rockwell model)

--- app/ranking_server_saumya_test.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Jun  8 15:43:15 2024
-
-@author: saumya
-"""
-
-from osomerank import audience_diversity
--- app/ranking_server_test.py
+++ app/ranking_server_test.py
@@ -5,7 +5,11 @@ sys.path.append(".")  # allows for importing from the current directory
 import pytest
 
 import ranking_server
-import sample_data
+import time
+import os
+import glob
+import json
+from datetime import datetime
 
 
 @pytest.fixture
@@ -20,30 +24,18 @@ def client(app):
 
 
 def test_rank(client):
+    fpath = "data/test_data/twitter_raw__2024-06-14T15:26:28.072Z.json"
+    payload = json.load(open(fpath))
+
+    start = time.time()
     # Send POST request to the API
-    response = client.post("/rank", json=sample_data.BASIC_EXAMPLE)
+    response = client.post("/rank", json=payload)
+
+    total_time = time.time() - start
+    print(f"Request took {total_time:.2f} seconds")
 
     # Check if the request was successful (status code 200)
     assert response.status_code == 200
-
-    # Check if the response is a dictionary
-    assert isinstance(response.json, dict)
-
-    # # Check if the response contains the expected ids, in the expected order
-    # assert response.json["ranked_ids"] == [
-    #     "571775f3-2564-4cf5-b01c-f4cb6bab461b",
-    #     "s5ad13266-8abk4-5219-kre5-2811022l7e43dv",
-    #     "a4c08177-8db2-4507-acc1-1298220be98d",
-    #     "de83fc78-d648-444e-b20d-853bf05e4f0e",
-    # ]
-
-    # # check for inserted posts
-    # assert response.json["new_items"] == [
-    #     {
-    #         "id": "571775f3-2564-4cf5-b01c-f4cb6bab461b",
-    #         "url": "https://reddit.com/r/PRCExample/comments/1f33ead/example_to_insert",
-    #     }
-    # ]
 
 
 if __name__ == "__main__":
