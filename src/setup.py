12e61e00146e9feba6100a3da72256f3e1b9f91a
Giovanni Luca Ciampaglia <glciampagl@gmail.com>
2024-07-02 17:46:29 -0400

    Updated pyproject

--- src/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name="osomerank",
-    version="1.0",
-    description="lib for ranking functions",
-    author="Bao Tran Truong <baotruon@iu.edu>, Saumya Bhadani <sbhadani@umd.edu>, Ozgur Can Seckin <oseckin@iu.edu>",
-    packages=find_packages(),
-)
